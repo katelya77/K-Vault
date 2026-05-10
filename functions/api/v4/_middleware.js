4de294138508f270ed793f6580c267ad408cf478
fouc3 <admin@1110103.xyz>
2026-05-11 05:59:56 +0800

    fix(api): 在CORS头中增加PATCH方法支持
    为了支持更多类型的HTTP请求，在跨域资源共享(CORS)配置中添加了PATCH方法。这允许客户端使用PATCH方法进行资源的部分更新。

--- functions/api/v4/_middleware.js
+++ functions/api/v4/_middleware.js
@@ -5,7 +5,7 @@
 
 function addCorsHeaders(headers) {
   headers.set('Access-Control-Allow-Origin', '*');
-  headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
   headers.set('Access-Control-Max-Age', '86400');
   return headers;
